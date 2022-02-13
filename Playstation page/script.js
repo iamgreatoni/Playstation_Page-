66874024b1883e0e184aefa84ef325ba1810e613
John Oni <onijohnoluwaseun@gmail.com>
2022-02-13 07:40:49 +0100

    Add files via upload

---
+++ Playstation page/script.js
@@ -0,0 +1,11 @@
+var menulist = document.getElementById("menulist");
+        menulist.style.maxHeight = "0px";
+
+        function togglemenu() {
+            if(menulist.style.maxHeight == "0px") {
+                menulist.style.maxHeight = "235px"
+            }
+            else {
+                menulist.style.maxHeight = "0px"
+            }
+        }
